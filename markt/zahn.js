e721242740186dc6b911020725acbefa4b8ba176
iki1uc <entertrainer@web.de>
2026-09-04 05:10:08 +0200

    Update zahn.js

--- markt/zahn.js
+++ markt/zahn.js
@@ -1,8 +1,2 @@
-const axis = A81_AXIS.build();
-
-axis.matrix.forEach(row => {
-    row.forEach(iqq => {
-        MassHWTranswarp.set(iqq, iqq, TimeHW.delta);
-        MassHWTranswarp.compute();
-    });
-});
+Gleichung.setStage(A81_AXIS.matrix[r][c]);
+const result = Gleichung.run();
